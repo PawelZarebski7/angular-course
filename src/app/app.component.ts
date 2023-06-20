4e814508732c788e322c9b103cd5e30fa4b9d487
Paweł <pawelzarebski7@wp.pl>
2023-06-20 23:35:21 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,53 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-new-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-project';
+  courseName: string;
+  author: string;
+  imgSrc: string;
+  competition: string;
+  workerName: string;
+  workerAge: number;
+  isTrue: boolean;
+  width: number;
+  values: number;
+  data = [
+    {
+      name: 'Pablo',
+      job: 'FullStack Dev',
+      age: 32
+    },
+    {
+      name: 'Eva',
+      job:  'Front-Dev',
+      age: 22
+    },
+    {
+      name: 'Izzak',
+      job: 'Senior Angular Dev',
+      age: 44
+    },
+    {
+      name: 'Luiza',
+      job: 'RoR Dev',
+      age: 36
+    }
+  ]
+
+  constructor() {
+    this.courseName = 'Angular'
+    this.author = 'Pablo Z.'
+    this.imgSrc = 'https://images.unsplash.com/photo-1654157925394-4b7809721149?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2060&q=80'
+    this.competition = 'New Competition 2023'
+    this.workerName = '';
+    this.workerAge = 0;
+    this.isTrue = false;
+    this.width = 2;
+    this.values = 1;
+
+  }
 }
